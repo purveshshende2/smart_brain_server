317fefcc56cf4b680fa6c0a1859700b23c931bd1
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-04-22 17:36:31 +0530

    updating database with process

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'dbms',
-      database : 'smart_brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
